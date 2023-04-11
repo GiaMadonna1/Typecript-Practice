21212c5ca2b34522ae556a1eeb423d2d35294956
GiaMadonna1 <109568865+GiaMadonna1@users.noreply.github.com>
2023-04-10 19:04:51 -0700

    Finished TS Basics activity

---
+++ build/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var utility_1 = require("./utility");
+var firstNumber = 2;
+var secondNumber = 3;
+var userName;
+userName = 'mrspotss';
+function showUser(user) {
+    return "Welcome, ".concat(user, "!");
+}
+console.log((0, utility_1.add)(firstNumber, secondNumber));
+console.log(showUser(userName));
